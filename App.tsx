cbf70e5bc9c3e12b72f7c7bed94c20bb7f3334f3
Khanh Dang <khanh.dang@ingka.com>
2025-11-14 00:10:23 +0200

    Test mqtt connection on Welcome screen

--- App.tsx
+++ App.tsx
@@ -10,12 +10,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from './src/navigation/navigators/RootNavigator';
 import { navigationRef } from './src/navigation/helpers/navigationRef';
+import { VesselMqttProvider } from './src/components/contexts/VesselMqttContext';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <NavigationContainer ref={navigationRef}>
-        <RootNavigator />
+        <VesselMqttProvider>
+          <RootNavigator />
+        </VesselMqttProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
